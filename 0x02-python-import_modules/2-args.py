9b3e46fcdec98d1edfc82326c277e4523e3fabf7
Jason Uzoukwu <theweirdjason@gmail.com>
2022-12-01 11:38:19 -0800

    committed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    args = len(sys.argv)
-    if(args <= 1):
-        print("{} arguments.".format(args - 1))
-    elif(args == 2):
-        print("{} argument:".format(args - 1))
-    else:
-        print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for idx, arg in enumerate(userin):
+        print("{:d}: {:s}".format(idx + 1, arg))
